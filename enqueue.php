0f8deb0711b2c71d57b10ce3b8d34be68685d7e3
Darnell Brown <darnell.brown@greystoneit.com>
2015-08-28 23:13:34 -0400

    Changed action to script enqueue

--- enqueue.php
+++ enqueue.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'wp_enqueue_style', 'google_roboto_style');
+add_action( 'wp_enqueue_scripts', 'google_roboto_style');
 function google_roboto_style() {
     wp_register_style( 'googlefont', 'http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300');
     wp_enqueue_style( 'googlefont' );
