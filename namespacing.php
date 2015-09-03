4919e9755f0854e90475a71557e3f2388ff66ef1
darnellbrown <darnell.brown@greystoneit.com>
2015-09-03 10:26:37 -0400

    Updated with suggested fixes

--- namespacing.php
+++ namespacing.php
@@ -5,14 +5,13 @@ namespace Foo_Bar;
 class Plugin {
 	public $config = array();
 	
-	public function __construct()
-	{
+	public function __construct() {
 		$config = [ 
-             "post_type_slug" => "foo", 
-             "taxonomy_slug" => "bar", 
-             "post_type_name" => "Foo", 
-             "taxonomy_name" => "Bar", 
-         ]; 
+			"post_type_slug" => "foo", 
+			"taxonomy_slug"  => "bar", 
+			"post_type_name" => "Foo", 
+			"taxonomy_name"  => "Bar", 
+		]; 
 	}
 }
 
